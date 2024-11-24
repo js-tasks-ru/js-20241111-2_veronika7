2903cd90de29540d41a86e18c7be8bdd742da042
v.murai <v.murai@skbkontur.ru>
2024-11-24 22:35:14 +0500

    Задачи 3 модуля

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function(obj) {
+    if (path && Object.keys(obj).length > 0) {
+      const splittedPath = path.split('.');
+      for (let i = 0; i < splittedPath.length; i++) {
+        obj = obj[splittedPath[i]];
+      }
+      return obj;
+    }
+    return undefined;
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let result = '';
+  let count = 0;
+  if (size === undefined) {
+    return string;
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1]) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count < size) {
+      result += string[i];
+    }
+  }
+  return result;
 }
